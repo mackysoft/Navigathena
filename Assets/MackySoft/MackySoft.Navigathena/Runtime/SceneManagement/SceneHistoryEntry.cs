d0d3042d791bbef8157d0c238227727fdb7579ca
mackysoft <mackysoft0129@gmail.com>
2023-10-27 01:23:30 +0900

    History make to generic

---
+++ Assets/MackySoft/MackySoft.Navigathena/Runtime/SceneManagement/SceneHistoryEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MackySoft.Navigathena.Transitions;
+
+namespace MackySoft.Navigathena.SceneManagement
+{
+
+	public interface IReadOnlySceneHistoryEntry
+	{
+		ISceneIdentifier Scene { get; }
+		ITransitionDirector TransitionDirector { get; }
+		ISceneDataReader DataReader { get; }
+	}
+
+	public sealed class SceneHistoryEntry : IReadOnlySceneHistoryEntry
+	{
+		public ISceneIdentifier Scene { get; }
+		public ITransitionDirector TransitionDirector { get; }
+		public SceneDataStore DataStore { get; }
+
+		ISceneDataReader IReadOnlySceneHistoryEntry.DataReader => DataStore.Reader;
+
+		public SceneHistoryEntry (ISceneIdentifier scene, ITransitionDirector transitionDirectorData, SceneDataStore sceneDataStore)
+		{
+			Scene = scene ?? throw new ArgumentNullException(nameof(scene));
+			TransitionDirector = transitionDirectorData;
+			DataStore = sceneDataStore ?? throw new ArgumentNullException(nameof(sceneDataStore));
+		}
+	}
+}
