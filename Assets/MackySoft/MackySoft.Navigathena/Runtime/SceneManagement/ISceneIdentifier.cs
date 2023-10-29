dcf6a8d7b661664144f8f2d56096d5ed5489785a
mackysoft <mackysoft0129@gmail.com>
2023-10-30 00:47:00 +0900

    Add SceneIdentifierExtensions

--- Assets/MackySoft/MackySoft.Navigathena/Runtime/SceneManagement/ISceneIdentifier.cs
+++ Assets/MackySoft/MackySoft.Navigathena/Runtime/SceneManagement/ISceneIdentifier.cs
@@ -19,4 +19,20 @@ namespace MackySoft.Navigathena.SceneManagement
 		UniTask<Scene> Load (IProgress<float> progress = null, CancellationToken cancellationToken = default);
 		UniTask Unload (IProgress<float> progress = null, CancellationToken cancellationToken = default);
 	}
+
+	public static class SceneIdentifierExtensions
+	{
+		public static ISceneIdentifier<T> AsTypedData<T> (this ISceneIdentifier identifier) where T : ISceneData
+		{
+			if (identifier == null)
+			{
+				throw new ArgumentNullException(nameof(identifier));
+			}
+			if (identifier is ISceneIdentifier<T> typedIdentifier)
+			{
+				return typedIdentifier;
+			}
+			return new TypedDataSceneIdentifier<T>(identifier);
+		}
+	}
 }
