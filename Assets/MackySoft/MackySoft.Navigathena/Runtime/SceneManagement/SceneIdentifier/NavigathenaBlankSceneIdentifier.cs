f4a4f5a30a0d987b122acaf60903e4cf5a503361
Makihiro <mackysoft0129@gmail.com>
2024-02-14 20:30:05 +0900

    Add NavigathenaBlankSceneIdentifier

---
+++ Assets/MackySoft/MackySoft.Navigathena/Runtime/SceneManagement/SceneIdentifier/NavigathenaBlankSceneIdentifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace MackySoft.Navigathena.SceneManagement
+{
+	internal sealed class NavigathenaBlankSceneIdentifier : ISceneIdentifier
+	{
+
+		public static readonly NavigathenaBlankSceneIdentifier Instance = new NavigathenaBlankSceneIdentifier();
+
+		public ISceneHandle CreateHandle ()
+		{
+			return NavigathenaBlankSceneHandle.Instance;
+		}
+
+		sealed class NavigathenaBlankSceneHandle : ISceneHandle
+		{
+
+			public static readonly NavigathenaBlankSceneHandle Instance = new NavigathenaBlankSceneHandle();
+
+			const string kSceneName = "Navigathena Blank";
+
+			public UniTask<Scene> Load (IProgress<float> progress = null, CancellationToken cancellationToken = default)
+			{
+				if (SceneManager.GetSceneByName(kSceneName).IsValid())
+				{
+					return UniTask.FromResult(SceneManager.GetSceneByName(kSceneName));
+				}
+				return UniTask.FromResult(SceneManager.CreateScene(kSceneName));
+			}
+
+			public async UniTask Unload (IProgress<float> progress = null, CancellationToken cancellationToken = default)
+			{
+				Scene scene = SceneManager.GetSceneByName(kSceneName);
+				if (!scene.isLoaded && !scene.IsValid())
+				{
+					return;
+				}
+
+				await SceneManager.UnloadSceneAsync(kSceneName)
+					.ToUniTask(progress, cancellationToken: cancellationToken);
+			}
+		}
+	}
+}
